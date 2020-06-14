3f0d5f2c62f6e41c6c48cec67ec8e8ceabaf2122
lcyjerry <lcy.jerry@qq.com>
2020-06-14 18:03:33 +0800

    轮播图

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Rank from 'components/rank/rank'
+import Recommend from 'components/recommend/recommend'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'/',
+    redirect:'/recommend'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/recommend',
+    component:Recommend
+  },
+  {
+    path:'/singer',
+    component:Singer
+  },
+  {
+    path:'/rank',
+    component:Rank
+  },
+  {
+    path:'/search',
+    component:Search
   }
 ]
 
---
+++ vue.config.js
@@ -0,0 +1,62 @@
+const path = require("path")
+const axios = require("axios")
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  devServer: {
+    before(app) {
+      app.get('/api/getTopBanner', function (req, res) {
+        const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+        const jumpPrefixMap = 'https://y.qq.com/n/yqq/album/'
+
+        axios.get(url, {
+          headers: {
+            referer: 'https://u.y.qq.com/',
+            host: 'u.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          response = response.data
+          if (response.code === 0) {
+            const slider = []
+            const content = response.focus.data && response.focus.data.content
+            if (content) {
+              for (let i = 0; i < content.length; i++) {
+                const item = content[i]
+                const sliderItem = {}
+                const jumpPrefix = jumpPrefixMap[item.type || 10002]
+                sliderItem.id = item.id
+                sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+                sliderItem.picUrl = item.pic_info.url
+                slider.push(sliderItem)
+              }
+            }
+            res.json({
+              code: 0,
+              data: {
+                slider
+              }
+            })
+          } else {
+            res.json(response)
+          }
+        }).catch((e) => {
+          console.log(e)
+        })
+      })
+    }
+  },
+
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("common", resolve("src/common"))
+      .set("components", resolve("src/components"))
+      .set("public", resolve("public"))
+      .set("api", resolve("src/api"))
+      .set("base", resolve("src/base"));
+  },
+}
