ed8564cf9948dc4adb1e5202650966a6243b5f4a
lcyjerry <lcy.jerry@qq.com>
2020-06-27 14:56:34 +0800

    debug

--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -76,8 +76,7 @@ export default {
 
 <style lang="stylus" scoped>
 .slide-enter-active, .slide-leave-active
-  transition all .3s
-
-.slide-enter, .slide-leave-top
-  transform translate3d(100%,0,0)
+  transition: all 0.3s ease
+.slide-enter, .slide-leave-to
+  transform: translate3d(100%,0,0)
 </style>
